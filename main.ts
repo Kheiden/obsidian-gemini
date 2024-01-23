25869209826910830e63524e60e8992d9433732c
Kurt Heiden <kurt.w.heiden@gmail.com>
2024-01-22 21:16:27 -0700

    Remove Configuration class and unneeded commands

--- main.ts
+++ main.ts
@@ -16,16 +16,6 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-	
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian Gemini', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello World');
-		});
-
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Gemini getting ready...');
@@ -38,16 +28,6 @@ export default class MyPlugin extends Plugin {
 			statusBarItemEl.setText('Gemini ready!');
 		}
 		
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-configuration-simple',
-			name: 'Open Configuration (simple)',
-			callback: () => {
-				new Configuration(this.app).open();
-			}
-		});
-
 		this.addCommand({
 			id: 'generate-content',
 			name: 'Generate Content',
@@ -87,35 +67,6 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new Configuration(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 
@@ -139,22 +90,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class Configuration extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
